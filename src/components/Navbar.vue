77df2fd9884b6983a062b978a4e4771654145a5f
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-01-10 17:02:41 -0200

    Adicionei o vue-router e o vue-material, configurei tudo, agora é só vencer a preguiça de fazer html, Deus me ajude

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <md-toolbar class="md-accent">
+    <h3 class="md-title">DANFY</h3>
+  </md-toolbar>
+</template>
+<script>
+export default {
+  name: "navbar",
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
