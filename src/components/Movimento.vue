6fc69343e0fa10e398ca98bc86d426c55443a282
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-01-12 09:50:19 -0200

    Extrai o emitente e o destinatário da nota

--- src/components/Movimento.vue
+++ src/components/Movimento.vue
@@ -40,7 +40,59 @@ export default {
 
           let info = obj.nfeProc.NFe.infNFe
 
-          console.log(info)
+          let notaId = info['_attributes'].Id.split('NFe')[1]
+
+          console.log(notaId)
+
+
+          let emit = info.emit
+          let emitenteId = emit.CNPJ['_text']
+          let emitente = {
+            nome: emit.xNome['_text'],
+            endereco: {
+              logradouro: emit.enderEmit.xLgr['_text'],
+              numero: emit.enderEmit.nro['_text'],
+              complemento: emit.enderEmit.xCpl ? emit.enderEmit.xCpl['_text'] : '',
+              bairro: emit.enderEmit.xBairro['_text'],
+              municipio: {
+                codigo: emit.enderEmit.cMun['_text'],
+                nome: emit.enderEmit.xMun['_text']
+              },
+              estado: emit.enderEmit.UF['_text'],
+              pais: {
+                codigo: emit.enderEmit.cPais['_text'],
+                nome: emit.enderEmit.xPais['_text']
+              },
+              cep: emit.enderEmit.CEP['_text']
+            }
+          }
+
+          console.log(emitente)
+
+          let dest = info.dest
+          let destId = dest.CPF ? dest.CPF['_text'] : dest.CNPJ['_text']
+          let destinatario = {
+            nome: dest.xNome['_text'],
+            endereco: {
+              logradouro: dest.enderDest.xLgr['_text'],
+              numero: dest.enderDest.nro['_text'],
+              complemento: dest.enderDest.xCpl ? dest.enderDest.xCpl['_text'] : '',
+              bairro: dest.enderDest.xBairro['_text'],
+              municipio: {
+                codigo: dest.enderDest.cMun['_text'],
+                nome: dest.enderDest.xMun['_text']
+              },
+              estado: dest.enderDest.UF['_text'],
+              pais: {
+                codigo: dest.enderDest.cPais['_text'],
+                nome: dest.enderDest.xPais['_text']
+              },
+              cep: dest.enderDest.CEP['_text']
+            }
+          }
+
+          console.log(destinatario)
+
 
         }
       }
