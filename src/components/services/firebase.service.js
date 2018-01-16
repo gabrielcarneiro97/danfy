670648f7bd3883655e5aa3f780f135857179ce8e
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-01-16 11:22:39 -0200

    Fiz uma função que valida um movimento, comparando CFOP, data e produtos de duas notas

--- src/components/services/firebase.service.js
+++ src/components/services/firebase.service.js
@@ -322,19 +322,71 @@ export function pegarTodasNotasPessoa (id, callback) {
 }
 
 export function compararCFOP (notaInicial, notaFinal) {
-  if ((notaFinal.geral.cfop === '5102' || notaFinal.geral.cfop === '6102' || notaFinal.geral.cfop === '6108') && (notaInicial.geral.cfop === '1102')) {
+  let cfopInicial = notaInicial.geral.cfop
+  let cfopFinal = notaFinal.geral.cfop
+
+  let cfopCompra = ['1102']
+  let cfopVenda = ['5102', '6102', '6108']
+  let cfopConsignacao = ['1917']
+  let cfopCompraConsignacao = ['1113']
+  let cfopVendaConsignacao = ['5115', '6115', '5114']
+  let cfopDevolucaoConsignacao = ['5918']
+
+  if (cfopCompra.includes(cfopInicial) && cfopVenda.includes(cfopFinal)) {
     return true
-  } else if ((notaFinal.geral.cfop === '5115' || notaFinal.geral.cfop === '6115') && (notaInicial.geral.cfop === '1113')) {
+  } else if (cfopCompraConsignacao.includes(cfopInicial) && cfopVendaConsignacao.include(cfopFinal)) {
     return true
-  } else if (notaFinal.geral.cfop === '1113' && notaInicial.geral.cfop === '1917') {
+  } else if (cfopConsignacao.includes(cfopInicial) && cfopCompraConsignacao.includes(cfopFinal)) {
     return true
-  } else if (notaFinal.geral.cfop === '5918' && notaInicial.geral.cfop === '1917') {
+  } else if (cfopConsignacao.includes(cfopInicial) && cfopDevolucaoConsignacao.includes(cfopFinal)) {
     return true
-  } else if ((notaFinal.emitente !== notaInicial.emitente) && (notaFinal.geral.cfop === '5102' || notaFinal.geral.cfop === '6102' || notaFinal.geral.cfop === '6108') && (notaInicial.geral.cfop === '5102' || notaInicial.geral.cfop === '6102' || notaInicial.geral.cfop === '6108')) {
+  } else if ((cfopVenda.includes(cfopInicial) && cfopVenda.include(cfopFinal)) && (notaFinal.emitente !== notaInicial.emitente)) {
     return true
   } else {
     return false
   }
+}
+
+export function compararProduto (notaInicial, notaFinal) {
+
+  let retorno = false
+
+  Object.keys(notaInicial.produtos).forEach(nomeProdutoInicial => {
+    Object.keys(notaFinal.produtos).forEach(nomeProdutoFinal => {
+      if (nomeProdutoFinal === nomeProdutoInicial) {
+        retorno = true
+      } else if (notaInicial.produtos[nomeProdutoInicial].descricao === notaFinal.produtos[nomeProdutoFinal].descricao) {
+        retorno = true
+      }
+    })
+  })
+
+  return retorno
+}
+
+export function compararData (notaInicial, notaFinal) {
+  let dataInicial = new Date(notaInicial.geral.dataHora).getTime()
+  let dataFinal = new Date(notaFinal.geral.dataHora).getTime()
+
+  if (dataInicial <= dataFinal) {
+    return true
+  } else {
+    return false
+  }
+}
+
+export function validarMovimento (notaInicial, notaFinal, callback) {
+  let err = null
+
+  if (!compararCFOP(notaInicial, notaFinal)) {
+    err = new Error(`O CFOP da Nota Inicial ${notaInicial.geral.cfop} não é valido para o CFOP da Nota Final ${notaFinal.geral.cfop}`)
+  } else if (!compararProduto(notaInicial, notaFinal)) {
+    err = new Error(`O produto da Nota Final não foi localizado na Nota Inicial!`)
+  } else if (!compararData(notaInicial, notaFinal)) {
+    err = new Error(`A data da Nota Final é anterior a data da Nota Inicial!`)
+  }
+
+  callback(err)
 }
 
 export function pegarDominio (callback) {
@@ -354,8 +406,6 @@ export function estaNoDominio (cnpj) {
   let retorno = false
 
   Object.keys(empresas).forEach(key => {
-    console.log(empresas[key])
-    console.log(cnpj)
     if (empresas[key] === cnpj) {
       retorno = true
     }
