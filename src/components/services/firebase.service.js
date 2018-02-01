eaffed7251f97fd533357d36af11b382e8f54671
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-02-01 11:38:44 -0200

    Comentei o firebase.service até a parte de pessoa

--- src/components/services/firebase.service.js
+++ src/components/services/firebase.service.js
@@ -18,10 +18,23 @@ var auth = firebase.auth()
 var db = firebase.database()
 
 //  INICIO FUNÇÕES RELACIONADAS A AUTENTICAÇÃO
+
+/*
+* {Function} entrar (login, senha, callback): responsável por entrar no sistema
+*   @param {String} login: contendo o e-mail do usuário
+*   @param {String} senha: contendo a senha do usuário
+*   @param {Function} callback (err, usuario): chamada ao fim da autenticação
+*     |-> @param {Error} err: contém um erro caso a autenticação falhe
+*     |-> @param {Object} usuario: contém os dados do usuário auntenticado
+*           |-> @attr {String} nome: contém o nome completo do usuário
+*           |-> @attr {String} login: contém o e-mail do usuário
+*           |-> @attr {String} dominio: contém o domínio do usuário, responsável por uma segunda camada de autenticação
+*/
 export function entrar (login, senha, callback) {
   auth.signInWithEmailAndPassword(login, senha).then(user => {
     db.ref('Usuarios/' + user.uid).once('value').then(value => {
       let usuario = value.val()
+      usuario.login = login
       db.ref('Dominios/' + usuario.dominio).once('value', snap => {
         storeVuex.commit(autenticar({ email: user.email, token: user.getIdToken(), id: user.uid, nome: usuario.nome, dominio: usuario.dominio, nivel: usuario.nivel }))
         if (snap.val()) {
@@ -41,17 +54,41 @@ export function entrar (login, senha, callback) {
     callback(err, null)
   })
 }
+
+/*
+* {Function} deslogar (callback): responsável por dar signout no usuário auntenticado
+*   @param {Function} callback (err): chamada ao fim do signout
+*     |-> @param {Error} err: contém um erro caso o signout falhe
+*/
 export function deslogar (callback) {
   auth.signOut().then(value => {
     storeVuex.commit(sair())
     callback()
-  })
+  }, err => { callback(err) })
 }
+
+/*
+* {Function} criarUsuario (dados, callback): responsável por criar os usuários no sistema, tanto no firebase.auth, quando no firebase.db. Autentica no final da criação
+*   @param {Object} dados: contém os dados do usuário
+*     |-> @attr {String} nome: contém o nome completo do usuário
+*     |-> @attr {String} login: contém o e-mail do usuário
+*     |-> @attr {String} senha: contém a senha do usuário
+*     |-> @attr {String} dominio: contém o domínio do usuário, responsável por uma segunda camada de autenticação
+*   @param {Function} callbac k(err, usuario): chamada ao final da criação do usuário
+*     |-> @param {Error} err: contém um erro caso a criação de usuário falhe
+*     |-> @param {Object} usuario: contém os dados do usuário criado
+*           |-> @attr {String} nome: contém o nome completo do usuário
+*           |-> @attr {String} login: contém o e-mail do usuário
+*           |-> @attr {String} dominio: contém o domínio do usuário, responsável por uma segunda camada de autenticação
+*/
 export function criarUsuario (dados, callback) {
   firebase.auth().createUserWithEmailAndPassword(dados.login, dados.senha).then(user => {
     storeVuex.commit(autenticar({ nome: dados.nome, dominio: dados.dominio, email: dados.login, token: user.getIdToken(), id: user.uid, nivel: 1 }))
-    let usuario = storeVuex.state.usuario
-
+    let usuario = {
+      nome: dados.nome,
+      login: dados.login,
+      dominio: dados.nome
+    }
     db.ref('Usuarios/' + usuario.id).set({
       nome: dados.nome,
       dominio: dados.dominio,
@@ -63,6 +100,17 @@ export function criarUsuario (dados, callback) {
     callback(err, null)
   })
 }
+
+/*
+* {Fucntion} usuarioAtivo (callback): checa se tem algum usuário autenticado no sistema
+*   @param {Function} callback (user, usuario, dominio.tipo): chamada após conferir se existe ou não usuário ativo
+*     |-> @param {Object} user: objeto recebido da consulta no auth, caso ele seja null significa que não existe usuário autenticado
+*     |-> @param {Object} usuario: contem as informações contidas no firebase.db do usuário
+*           |-> @attr {String} nome: contém o nome completo do usuário
+*           |-> @attr {String} nivel: contém o nível de permissões do úsuario
+*           |-> @attr {String} dominio: contém o domínio do usuário, responsável por uma segunda camada de autenticação
+*     |-> @param {String} dominio.tipo: contém o tipo do domíno, podendo ser 'mult' ou 'unico'
+*/
 export function usuarioAtivo (callback) {
   auth.onAuthStateChanged(user => {
     if (user) {
@@ -86,6 +134,15 @@ export function usuarioAtivo (callback) {
     }
   })
 }
+
+/*
+* {Function} trocarSenha (senhaNova, senhaAntiga, login, callback): troca a senha do usuário autenticado, antes, porém, reautentica o usuário
+*   @param {String} senhaNova: contém a nova senha do usuário
+*   @param {String} senhaAntiga: contém a senha antiga do usuário
+*   @param {String} login: contém o e-mail do usuário
+*   @param {Function} callback (err): função chamada após a troca da senha
+*     |-> @param {Error} err: contém um erro informando se a troca foi bem sucedida ou não
+*/
 export function trocarSenha (senhaNova, senhaAntiga, login, callback) {
   auth.signInWithEmailAndPassword(login, senhaAntiga).then(user => {
     user.updatePassword(senhaNova).then(() => {
@@ -97,6 +154,15 @@ export function trocarSenha (senhaNova, senhaAntiga, login, callback) {
     console.error(err)
   })
 }
+
+/*
+* {Function} alterarDadoUsuario (dado, campo, id, callback): troca um dado do usuário, podendo ser nome ou domínio
+*   @param {String} dado: contém a nova informação do campo
+*   @param {String} campo: contém o campo que vai ser alterado
+*   @param {String} id: contém o id único do usuário
+*   @param {Function} callback (err): função chamada após a troca do dado
+*     |-> @ param {Error} err: chama erro caso não tenha sucesso em trocar o campo
+*/
 export function alterarDadoUsuario (dado, campo, id, callback) {
   db.ref('Usuarios/' + id + '/' + campo).set(dado, err => {
     if (err) {
@@ -111,9 +177,16 @@ export function alterarDadoUsuario (dado, campo, id, callback) {
     }
   })
 }
+
 // FIM DAS FUNÇÕES RELACIONADAS A AUTENTICAÇÃO
 
 // ACESSO DB PESSOAS
+
+/*
+* {Function} gravarPessoas (callback): pega as pessoas no store e grava no firebase.db
+*   @param {Function} callback (err): chamado após gravar as pessoas no firebase.db
+*     |-> @param {Error} err> contém um erro caso alguma coisa saia errado
+*/
 export function gravarPessoas (callback) {
   let pessoas = storeVuex.state.pessoas
   let keys = Object.keys(pessoas)
@@ -131,11 +204,55 @@ export function gravarPessoas (callback) {
     callback(null)
   }
 }
+
+/*
+* {Function} gravarPessoa (id, pessoa, callback): recebe uma pessoa específica e grava no firebase.db
+*   @param {String} id: contém o id da pessoa, podendo ser CPF ou CNPJ
+*   @param {Object} pessoa: contém os dados da pessoa, exatamente como é esperado pelo firebase.db
+*     |-> @attr {String} nome: contém o nome ou razão social da pessoa
+*     |-> @attr {Object} endereco: contém o endereço da pessoa
+*           |-> @attr {String} logradouro: contém o logradouro do endereço
+*           |-> @attr {String} numero: contém o número do endereço
+*           |-> @attr {String} complemento: contém o complemento do endereço
+*           |-> @attr {String} bairro: contém o bairro do endereço
+*           |-> @attr {String} estado: contém a sigla do estado do endereço
+*           |-> @attr {String} cep: contém o CEP do endereço
+*           |-> @attr {Object} municipio: contém o munícipio do endereço
+*                 |-> @attr {String} codigo: contém o código IBGE do municípo
+*                 |-> @attr {String} nome: contém o nome da cidade
+*           |-> @attr {Object} pais: contém o país do endereço
+*                 |-> @attr {String} codigo: contém o código do país
+*                 |-> @attr {String} nome: contém o nome do país
+*   @param {Function} callback (err): chamado após a gravação
+*     |-> @param {Error} err: contém um erro caso a gravação dê errado
+*/
 export function gravarPessoa (id, pessoa, callback) {
   db.ref('Pessoas/' + id).set(pessoa, err => {
     callback(err)
   })
 }
+
+/*
+* {Function} pegarPessoaId (id, callback): recupera uma pessoa do firebase.db ou do store baseado no id (CPF ou CNPJ)
+*   |-> @param {String} id: CPF ou CNPJ da pessoa a ser recuperada
+*   |-> @param {Function} callback (err, pessoa): função chamada após a recuperação dos dados
+*         |-> @param {Error} err: contém um erro caso a recuperação falhe
+*         |-> @param {Object} pessoa: contém os dados da pessoa encontrados no firebase.db ou no store
+*               |-> @attr {String} nome: contém o nome ou razão social da pessoa
+*               |-> @attr {Object} endereco: contém o endereço da pessoa
+*                    |-> @attr {String} logradouro: contém o logradouro do endereço
+*                     |-> @attr {String} numero: contém o número do endereço
+*                     |-> @attr {String} complemento: contém o complemento do endereço
+*                     |-> @attr {String} bairro: contém o bairro do endereço
+*                     |-> @attr {String} estado: contém a sigla do estado do endereço
+*                     |-> @attr {String} cep: contém o CEP do endereço
+*                     |-> @attr {Object} municipio: contém o munícipio do endereço
+*                           |-> @attr {String} codigo: contém o código IBGE do municípo
+*                           |-> @attr {String} nome: contém o nome da cidade
+*                     |-> @attr {Object} pais: contém o país do endereço
+*                           |-> @attr {String} codigo: contém o código do país
+*                           |-> @attr {String} nome: contém o nome do país
+*/
 export function pegarPessoaId (id, callback) {
   let pessoa
   let storeVuexPessoas = storeVuex.state.pessoas
