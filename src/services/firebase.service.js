71cb6455c76bd3b9dbd2fdf5e9247740fcbb3fe1
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-05-24 12:05:48 -0300

    Adicionei alguns arquivos do firebase

---
+++ src/services/firebase.service.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+import { firebaseConfig } from './private';
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+export function loginGoogle(options) {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  return firebase.auth().signInWithPopup(provider);
+
+}
