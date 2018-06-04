03629442d423492efae049d8a7531e226d45857d
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-06-04 14:53:25 -0300

    Refatorei AliquotasEmpresa

--- src/components/AliquotasEmpresa.jsx
+++ src/components/AliquotasEmpresa.jsx
@@ -85,6 +85,7 @@ class AliquotasEmpresa extends React.Component {
       visible: true,
     });
   }
+
   handleOk = () => {
     if (parseInt(this.state.numero, 10) > 0 && parseFloat(this.state.impostosEmpresa.iss) >= 0) {
       confirm({
@@ -120,12 +121,98 @@ class AliquotasEmpresa extends React.Component {
       message.error('Número e ISS devem ser preenchidos!');
     }
   }
-  handleCancel = (e) => {
-    console.log(e);
+  handleCancel = () => {
     this.setState({
       visible: false,
     });
   }
+
+  informacoesGeraisForm = () => (
+    <React.Fragment>
+      <Divider>Informações Gerais</Divider>
+      <Row className="row">
+        <Col span={12}>
+          <Input addonBefore="Número" defaultValue={this.state.numero} onChange={this.setNumero} />
+        </Col>
+        <Col span={12}>
+          <Select onChange={this.setTributacao} defaultValue={this.state.tributacao} style={{ width: '100%' }}>
+            <Option value="LP">Lucro Presumido</Option>
+            <Option value="SN">Simples Nacional</Option>
+          </Select>
+        </Col>
+      </Row>
+      <Row className="row">
+        <Col span={12} style={{ marginTop: '5px', marginBottom: '5px' }}>
+          <Checkbox onChange={this.setLiminar}>Liminar de Redução</Checkbox>
+        </Col>
+        <Col span={12}>
+          <Select onChange={this.setformaPagamentoTrimestrais} defaultValue={this.state.formaPagamentoTrimestrais} style={{ width: '100%' }}>
+            <Option value="adiantamento">Adiantamento</Option>
+            <Option value="acumulado">Acumulado por Trimestre</Option>
+            <Option value="cotas">Pagamento em Cotas</Option>
+          </Select>
+        </Col>
+      </Row>
+    </React.Fragment>
+  )
+
+  impostosForm = impostosEmpresa => (
+    <div>
+      <Divider dashed>Impostos Federais</Divider>
+      <Row className="row">
+        <Col span={12}>
+          <Input addonBefore="IRPJ" value={impostosEmpresa.irpj} disabled />
+        </Col>
+        <Col span={12}>
+          <Input addonBefore="CSLL" value={impostosEmpresa.csll} disabled />
+        </Col>
+      </Row>
+      <Row className="row">
+        <Col span={12}>
+          <Input addonBefore="PIS" value={impostosEmpresa.pis} disabled />
+        </Col>
+        <Col span={12}>
+          <Input addonBefore="COFINS" value={impostosEmpresa.cofins} disabled />
+        </Col>
+      </Row>
+      <Divider dashed>Impostos Estaduais (ICMS)</Divider>
+      <Row className="row">
+        <Col span={12}>
+          <Input addonBefore="Aliquota" value={impostosEmpresa.icms.aliquota} disabled />
+        </Col>
+        <Col span={12}>
+          <Input addonBefore="Redução" value={impostosEmpresa.icms.reducao} disabled />
+        </Col>
+      </Row>
+      <Divider dashed>Impostos Municipais</Divider>
+      <Row className="row">
+        <Col span={24}>
+          <Input addonBefore="ISS" defaultValue={impostosEmpresa.iss} onChange={this.setIss} />
+        </Col>
+      </Row>
+    </div>
+  )
+
+  modalRender = (dados, impostosEmpresa) => (
+    <Modal
+      title={dados.nome}
+      visible={this.state.visible}
+      onOk={this.handleOk}
+      onCancel={this.handleCancel}
+      style={{
+        marginTop: '40px',
+        position: 'static',
+      }}
+    >
+      {this.informacoesGeraisForm()}
+      {
+        this.state.tributacao === 'LP'
+        &&
+        this.impostosForm(impostosEmpresa)
+      }
+    </Modal>
+  );
+
   render() {
     const { dados } = this.props;
     const { impostosEmpresa } = this.state;
@@ -133,79 +220,7 @@ class AliquotasEmpresa extends React.Component {
     return (
       <div>
         <Button onClick={this.showModal} size="small">Adicionar</Button>
-        <Modal
-          title={dados.nome}
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-          style={{
-            marginTop: '40px',
-            position: 'static',
-          }}
-        >
-          <Divider>Informações Gerais</Divider>
-          <Row className="row">
-            <Col span={12}>
-              <Input addonBefore="Número" defaultValue={this.state.numero} onChange={this.setNumero} />
-            </Col>
-            <Col span={12}>
-              <Select onChange={this.setTributacao} defaultValue={this.state.tributacao} style={{ width: '100%' }}>
-                <Option value="LP">Lucro Presumido</Option>
-                <Option value="SN">Simples Nacional</Option>
-              </Select>
-            </Col>
-          </Row>
-          <Row className="row">
-            <Col span={12} style={{ marginTop: '5px', marginBottom: '5px' }}>
-              <Checkbox onChange={this.setLiminar}>Liminar de Redução</Checkbox>
-            </Col>
-            <Col span={12}>
-              <Select onChange={this.setformaPagamentoTrimestrais} defaultValue={this.state.formaPagamentoTrimestrais} style={{ width: '100%' }}>
-                <Option value="adiantamento">Adiantamento</Option>
-                <Option value="acumulado">Acumulado por Trimestre</Option>
-                <Option value="cotas">Pagamento em Cotas</Option>
-              </Select>
-            </Col>
-          </Row>
-          {
-            this.state.tributacao === 'LP'
-            &&
-            <div>
-              <Divider dashed>Impostos Federais</Divider>
-              <Row className="row">
-                <Col span={12}>
-                  <Input addonBefore="IRPJ" value={impostosEmpresa.irpj} disabled />
-                </Col>
-                <Col span={12}>
-                  <Input addonBefore="CSLL" value={impostosEmpresa.csll} disabled />
-                </Col>
-              </Row>
-              <Row className="row">
-                <Col span={12}>
-                  <Input addonBefore="PIS" value={impostosEmpresa.pis} disabled />
-                </Col>
-                <Col span={12}>
-                  <Input addonBefore="COFINS" value={impostosEmpresa.cofins} disabled />
-                </Col>
-              </Row>
-              <Divider dashed>Impostos Estaduais (ICMS)</Divider>
-              <Row className="row">
-                <Col span={12}>
-                  <Input addonBefore="Aliquota" value={impostosEmpresa.icms.aliquota} disabled />
-                </Col>
-                <Col span={12}>
-                  <Input addonBefore="Redução" value={impostosEmpresa.icms.reducao} disabled />
-                </Col>
-              </Row>
-              <Divider dashed>Impostos Municipais</Divider>
-              <Row className="row">
-                <Col span={24}>
-                  <Input addonBefore="ISS" defaultValue={impostosEmpresa.iss} onChange={this.setIss} />
-                </Col>
-              </Row>
-            </div>
-          }
-        </Modal>
+        {this.modalRender(dados, impostosEmpresa)}
       </div>
     );
   }
