9c6ac24776dff7d6683fafe74b3d488dde4b050c
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-06-06 09:06:10 -0300

    mudei o link da api

--- src/services/publics.js
+++ src/services/publics.js
@@ -1,3 +1,3 @@
 export { version } from '../../package.json';
 // export const api = 'http://localhost:8080';
-export const api = 'http://ec2-52-14-85-141.us-east-2.compute.amazonaws.com:8080';
+export const api = 'https://ec2-18-217-167-30.us-east-2.compute.amazonaws.com:8080';
