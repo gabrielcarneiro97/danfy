7e4a1e2efb4d129a86a9e5fd04f148a2d0e880ae
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-06-08 10:26:51 -0300

    Arrumei alguns erros de AcumuladosTable

--- src/components/AcumuladosTable.jsx
+++ src/components/AcumuladosTable.jsx
@@ -5,40 +5,38 @@ import { Table, Row, Col } from 'antd';
 import { pegaMes, R$ } from '../services';
 
 function defineDataSource(props) {
-    const { trimestre } = props.dados;
-    const dataSource = [];
+  const { trimestre } = props.dados;
+  const dataSource = [];
 
-    Object.keys(trimestre).forEach((key) => {
-      if (key === 'totais') {
-        const mes = <strong>Trimestre</strong>;
-        const el = trimestre[key];
+  Object.keys(trimestre).forEach((key) => {
+    if (key === 'totais') {
+      const mes = <strong>Trimestre</strong>;
+      const el = trimestre[key];
 
-        dataSource.push({
-          key: `acumulado-${key}`,
-          mes,
-          csll: <strong>{R$(el.impostos.csll - el.impostos.retencoes.csll)}</strong>,
-          irpj: <strong>{R$(el.impostos.irpj - el.impostos.retencoes.irpj)}</strong>,
-          faturamento: <strong>{R$(el.lucro + el.servicos)}</strong>,
-        });
-      } else {
-        const mes = pegaMes(key);
-        const el = trimestre[key];
+      dataSource.push({
+        key: `acumulado-${key}`,
+        mes,
+        csll: <strong>{R$(el.impostos.csll - el.impostos.retencoes.csll)}</strong>,
+        irpj: <strong>{R$(el.impostos.irpj - el.impostos.retencoes.irpj)}</strong>,
+        faturamento: <strong>{R$(el.lucro + el.servicos)}</strong>,
+      });
+    } else {
+      const mes = pegaMes(key);
+      const el = trimestre[key];
 
-        dataSource.push({
-          key: `acumulado-${key}`,
-          mes,
-          csll: R$(el.totais.impostos.csll - el.totais.impostos.retencoes.csll),
-          irpj: R$(el.totais.impostos.irpj - el.totais.impostos.retencoes.irpj),
-          faturamento: R$(el.totais.lucro + el.totais.servicos),
-        });
-      }
-    });
-
-    return dataSource;
-  }
+      dataSource.push({
+        key: `acumulado-${key}`,
+        mes,
+        csll: R$(el.totais.impostos.csll - el.totais.impostos.retencoes.csll),
+        irpj: R$(el.totais.impostos.irpj - el.totais.impostos.retencoes.irpj),
+        faturamento: R$(el.totais.lucro + el.totais.servicos),
+      });
+    }
+  });
+  return dataSource;
+}
 
 function AcumuladosTable(props) {
-
   const dataSource = defineDataSource(props);
 
   return (
@@ -60,14 +58,13 @@ function AcumuladosTable(props) {
       </Col>
     </Row>
   );
-
 }
 
 AcumuladosTable.propTypes = {
-  dados: PropTypes.shape({
+  dados: PropTypes.shape({ // eslint-disable-line
     trimestre: PropTypes.object,
   }).isRequired,
-}
+};
 
 AcumuladosTable.columns = [{
   title: 'Mês',
