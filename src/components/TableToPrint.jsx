8ef277a77234b64cae08fa6266c14f9c6778c013
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-06-08 10:48:52 -0300

    converti TableToPrint para um componente funcional

--- src/components/TableToPrint.jsx
+++ src/components/TableToPrint.jsx
@@ -2,87 +2,83 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './TableToPrint.css';
 
-class TableToPrint extends React.Component {
-  static propTypes = {
-    columns: PropTypes.array, // eslint-disable-line
-    dataSource: PropTypes.array, // eslint-disable-line
+function TableToPrint(props) {
+  const { columns, dataSource } = props;
+
+  if (dataSource.length === 0) {
+    return <div />;
   }
 
-  state = {}
+  const headRows = [];
+  const keyOrder = [];
+  headRows[0] = [];
+  headRows[1] = [];
+  const dataRows = [];
 
-  render() {
-    const { columns, dataSource } = this.props;
-
-    if (dataSource.length === 0) {
-      return <div />;
+  columns.forEach((el) => {
+    if (el.children) {
+      headRows[0].push({
+        hasChildren: true,
+        key: el.title,
+        content: <th key={el.title} colSpan={el.children.length}>{el.title}</th>,
+      });
+      el.children.forEach((child) => {
+        keyOrder.push(child.key);
+        headRows[1].push({
+          key: child.key,
+          content: <th key={child.key}>{child.title}</th>,
+        });
+      });
+    } else if (el.key !== 'editar') {
+      keyOrder.push(el.key);
+      headRows[0].push({
+        key: el.key,
+        content: <th key={el.key} rowSpan="2">{el.title}</th>,
+      });
     }
+  });
 
-    const headRows = [];
-    const keyOrder = [];
-    headRows[0] = [];
-    headRows[1] = [];
-    const dataRows = [];
+  dataSource.forEach((element, num) => {
+    dataRows[num] = [];
+    if (element) {
+      Object.keys(element).forEach((key) => {
+        const index = keyOrder.indexOf(key);
+        dataRows[num][index] = <td key={`${element.key}-${key}`}>{element[key]}</td>;
+      });
+    }
+  });
 
-    columns.forEach((el) => {
-      if (el.children) {
-        headRows[0].push({
-          hasChildren: true,
-          key: el.title,
-          content: <th key={el.title} colSpan={el.children.length}>{el.title}</th>,
-        });
-        el.children.forEach((child) => {
-          keyOrder.push(child.key);
-          headRows[1].push({
-            key: child.key,
-            content: <th key={child.key}>{child.title}</th>,
+  return (
+    <table className="table-print">
+      <thead>
+        {(() => {
+          const rows = [];
+          headRows.forEach((row, id) => {
+            const each = [];
+            row.forEach((r) => {
+              each.push(r.content);
+            });
+            rows.push(<tr key={`${id}-head-row`}>{each}</tr>); // eslint-disable-line
           });
-        });
-      } else if (el.key !== 'editar') {
-        keyOrder.push(el.key);
-        headRows[0].push({
-          key: el.key,
-          content: <th key={el.key} rowSpan="2">{el.title}</th>,
-        });
-      }
-    });
-
-    dataSource.forEach((element, num) => {
-      dataRows[num] = [];
-      if (element) {
-        Object.keys(element).forEach((key) => {
-          const index = keyOrder.indexOf(key);
-          dataRows[num][index] = <td key={`${element.key}-${key}`}>{element[key]}</td>;
-        });
-      }
-    });
-
-    return (
-      <table className="table-print">
-        <thead>
-          {(() => {
-            const rows = [];
-            headRows.forEach((row, id) => {
-              const each = [];
-              row.forEach((r) => {
-                each.push(r.content);
-              });
-              rows.push(<tr key={`${id}-head-row`}>{each}</tr>); // eslint-disable-line
-            });
-            return rows;
-          })()}
-        </thead>
-        <tbody>
-          {(() => {
-            const rows = [];
-            dataRows.forEach((row, id) => {
-              rows.push(<tr key={`${id}-data-row`}>{row}</tr>); // eslint-disable-line
-            });
-            return rows;
-          })()}
-        </tbody>
-      </table>
-    );
-  }
+          return rows;
+        })()}
+      </thead>
+      <tbody>
+        {(() => {
+          const rows = [];
+          dataRows.forEach((row, id) => {
+            rows.push(<tr key={`${id}-data-row`}>{row}</tr>); // eslint-disable-line
+          });
+          return rows;
+        })()}
+      </tbody>
+    </table>
+  );
 }
+
+TableToPrint.propTypes = {
+  columns: PropTypes.array, // eslint-disable-line
+  dataSource: PropTypes.array, // eslint-disable-line
+};
 
 export default TableToPrint;
