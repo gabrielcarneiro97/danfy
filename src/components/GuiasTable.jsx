64973cf957eb61630777249aac0b92cb7a848229
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-06-08 10:26:01 -0300

    Converti GuiasTable em componente funcional

--- src/components/GuiasTable.jsx
+++ src/components/GuiasTable.jsx
@@ -4,154 +4,150 @@ import { Row, Col, Table } from 'antd';
 
 import { R$ } from '../services';
 
-class GuiasTable extends React.Component {
-  static propTypes = {
-    dados: PropTypes.shape({
-      servicos: PropTypes.object,
-      movimentos: PropTypes.object,
-      complementares: PropTypes.object,
-      trimestre: PropTypes.object,
-    }).isRequired,
+const temServicos = props => Object.keys(props.dados.servicos).length > 0;
+const temMovimentos = props => Object.keys(props.dados.movimentos).length > 0;
+
+function gerarTable(props) {
+  const { complementares, trimestre } = props.dados;
+  const columns = [];
+  const dataSource = [];
+  const data = {};
+
+  if (temMovimentos(props)) {
+    columns.push({
+      title: 'ICMS',
+      dataIndex: 'icms',
+      key: 'icms',
+    });
+    data.icms = R$(trimestre[complementares.mes].totais.impostos.icms.proprio +
+      trimestre[complementares.mes].totais.impostos.icms.difal.origem);
   }
 
-  state = {}
+  if (temServicos(props)) {
+    columns.push({
+      title: 'ISS',
+      dataIndex: 'iss',
+      key: 'iss',
+    });
+    data.iss = R$(trimestre[complementares.mes].totais.impostos.iss -
+      trimestre[complementares.mes].totais.impostos.retencoes.iss);
+  }
 
-  temServicos = () => Object.keys(this.props.dados.servicos).length > 0;
-  temMovimentos = () => Object.keys(this.props.dados.movimentos).length > 0;
+  columns.push({
+    title: 'PIS',
+    dataIndex: 'pis',
+    key: 'pis',
+  });
 
-  gerarTable = () => {
-    const { complementares, trimestre } = this.props.dados;
-    const columns = [];
-    const dataSource = [];
-    const data = {};
+  data.pis = R$((trimestre[complementares.mes].totais.impostos.pis -
+    trimestre[complementares.mes].totais.impostos.retencoes.pis) +
+    trimestre[complementares.mes].totais.impostos.acumulado.pis);
 
-    if (this.temMovimentos()) {
-      columns.push({
-        title: 'ICMS',
-        dataIndex: 'icms',
-        key: 'icms',
-      });
-      data.icms = R$(trimestre[complementares.mes].totais.impostos.icms.proprio +
-        trimestre[complementares.mes].totais.impostos.icms.difal.origem);
-    }
+  columns.push({
+    title: 'COFINS',
+    dataIndex: 'cofins',
+    key: 'cofins',
+  });
 
-    if (this.temServicos()) {
-      columns.push({
-        title: 'ISS',
-        dataIndex: 'iss',
-        key: 'iss',
-      });
-      data.iss = R$(trimestre[complementares.mes].totais.impostos.iss -
-        trimestre[complementares.mes].totais.impostos.retencoes.iss);
-    }
+  data.cofins = R$((trimestre[complementares.mes].totais.impostos.cofins -
+    trimestre[complementares.mes].totais.impostos.retencoes.cofins) +
+    trimestre[complementares.mes].totais.impostos.acumulado.cofins);
+
+  if (parseInt(complementares.mes, 10) % 3 === 0 &&
+    complementares.formaPagamento !== 'PAGAMENTO ANTECIPADO') {
+    columns.push({
+      title: 'CSLL',
+      dataIndex: 'csll',
+      key: 'csll',
+    });
+    data.csll = R$(trimestre.totais.impostos.csll - trimestre.totais.impostos.retencoes.csll);
 
     columns.push({
-      title: 'PIS',
-      dataIndex: 'pis',
-      key: 'pis',
+      title: 'IRPJ + ADICIONAL',
+      dataIndex: 'irpj',
+      key: 'irpj',
     });
-
-    data.pis = R$((trimestre[complementares.mes].totais.impostos.pis -
-      trimestre[complementares.mes].totais.impostos.retencoes.pis) +
-      trimestre[complementares.mes].totais.impostos.acumulado.pis);
+    data.irpj = R$((trimestre.totais.impostos.irpj -
+      trimestre.totais.impostos.retencoes.irpj) +
+      trimestre.totais.impostos.adicionalIr);
+  } else if (parseInt(complementares.mes, 10) % 3 !== 0 &&
+    complementares.formaPagamento === 'PAGAMENTO ANTECIPADO') {
+    columns.push({
+      title: 'CSLL',
+      dataIndex: 'csll',
+      key: 'csll',
+    });
+    data.csll = R$(trimestre[complementares.mes].totais.impostos.csll -
+      trimestre[complementares.mes].totais.impostos.retencoes.csll);
 
     columns.push({
-      title: 'COFINS',
-      dataIndex: 'cofins',
-      key: 'cofins',
+      title: 'IRPJ',
+      dataIndex: 'irpj',
+      key: 'irpj',
     });
+    data.irpj = R$(trimestre[complementares.mes].totais.impostos.irpj -
+      trimestre[complementares.mes].totais.impostos.retencoes.irpj);
+  } else if (parseInt(complementares.mes, 10) % 3 === 0 &&
+    complementares.formaPagamento === 'PAGAMENTO ANTECIPADO') {
+    columns.push({
+      title: 'CSLL',
+      dataIndex: 'csll',
+      key: 'csll',
+    });
+    data.csll = R$(trimestre[complementares.mes].totais.impostos.csll -
+      trimestre[complementares.mes].totais.impostos.retencoes.csll);
 
-    data.cofins = R$((trimestre[complementares.mes].totais.impostos.cofins -
-      trimestre[complementares.mes].totais.impostos.retencoes.cofins) +
-      trimestre[complementares.mes].totais.impostos.acumulado.cofins);
-
-    if (parseInt(complementares.mes, 10) % 3 === 0 &&
-      complementares.formaPagamento !== 'PAGAMENTO ANTECIPADO') {
-      columns.push({
-        title: 'CSLL',
-        dataIndex: 'csll',
-        key: 'csll',
-      });
-      data.csll = R$(trimestre.totais.impostos.csll - trimestre.totais.impostos.retencoes.csll);
-
-      columns.push({
-        title: 'IRPJ + ADICIONAL',
-        dataIndex: 'irpj',
-        key: 'irpj',
-      });
-      data.irpj = R$((trimestre.totais.impostos.irpj -
-        trimestre.totais.impostos.retencoes.irpj) +
-        trimestre.totais.impostos.adicionalIr);
-    } else if (parseInt(complementares.mes, 10) % 3 !== 0 &&
-      complementares.formaPagamento === 'PAGAMENTO ANTECIPADO') {
-      columns.push({
-        title: 'CSLL',
-        dataIndex: 'csll',
-        key: 'csll',
-      });
-      data.csll = R$(trimestre[complementares.mes].totais.impostos.csll -
-        trimestre[complementares.mes].totais.impostos.retencoes.csll);
-
-      columns.push({
-        title: 'IRPJ',
-        dataIndex: 'irpj',
-        key: 'irpj',
-      });
-      data.irpj = R$(trimestre[complementares.mes].totais.impostos.irpj -
-        trimestre[complementares.mes].totais.impostos.retencoes.irpj);
-    } else if (parseInt(complementares.mes, 10) % 3 === 0 &&
-      complementares.formaPagamento === 'PAGAMENTO ANTECIPADO') {
-      columns.push({
-        title: 'CSLL',
-        dataIndex: 'csll',
-        key: 'csll',
-      });
-      data.csll = R$(trimestre[complementares.mes].totais.impostos.csll -
-        trimestre[complementares.mes].totais.impostos.retencoes.csll);
-
-      columns.push({
-        title: 'IRPJ + ADICIONAL',
-        dataIndex: 'irpj',
-        key: 'irpj',
-      });
-      data.irpj = R$((trimestre[complementares.mes].totais.impostos.irpj -
-        trimestre[complementares.mes].totais.impostos.retencoes.irpj) +
-        trimestre.totais.impostos.adicionalIr);
-    }
-
-    data.key = 'guias';
-
-    dataSource.push(data);
-
-    return {
-      dataSource,
-      columns,
-    };
+    columns.push({
+      title: 'IRPJ + ADICIONAL',
+      dataIndex: 'irpj',
+      key: 'irpj',
+    });
+    data.irpj = R$((trimestre[complementares.mes].totais.impostos.irpj -
+      trimestre[complementares.mes].totais.impostos.retencoes.irpj) +
+      trimestre.totais.impostos.adicionalIr);
   }
 
-  render() {
-    const { dataSource, columns } = this.gerarTable();
+  data.key = 'guias';
 
-    return (
-      <Row
-        type="flex"
-        justify="center"
-      >
-        <Col span={23}>
-          <Table
-            bordered
-            size="small"
-            columns={columns}
-            dataSource={dataSource}
-            pagination={{ position: 'none' }}
-            style={{
-              marginBottom: '20px',
-            }}
-          />
-        </Col>
-      </Row>
-    );
-  }
+  dataSource.push(data);
+
+  return {
+    dataSource,
+    columns,
+  };
 }
+
+function GuiasTable(props) {
+  const { dataSource, columns } = gerarTable(props);
+
+  return (
+    <Row
+      type="flex"
+      justify="center"
+    >
+      <Col span={23}>
+        <Table
+          bordered
+          size="small"
+          columns={columns}
+          dataSource={dataSource}
+          pagination={{ position: 'none' }}
+          style={{
+            marginBottom: '20px',
+          }}
+        />
+      </Col>
+    </Row>
+  );
+}
+
+GuiasTable.propTypes = {
+  dados: PropTypes.shape({ // eslint-disable-line
+    servicos: PropTypes.object,
+    movimentos: PropTypes.object,
+    complementares: PropTypes.object,
+    trimestre: PropTypes.object,
+  }).isRequired,
+};
 
 export default GuiasTable;
