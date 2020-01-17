1cb93eab1e119bb1638283e768eca219f42e3d14
Gabriel Carneiro <gabriel.carneiro97@live.com>
2020-01-17 08:22:40 -0300

    new ver

--- src/services/publics.ts
+++ src/services/publics.ts
@@ -1,4 +1,4 @@
 export { version } from '../../package.json';
-export const api = process.env.NODE_ENV === 'production'
-  ? 'https://api.danfy.online:8080/danfy' : 'http://localhost:8080/api';
-// export const api = 'https://api.danfy.online:8080/danfy';
+// export const api = process.env.NODE_ENV === 'production'
+//   ? 'https://api.danfy.online:8080/danfy' : 'http://localhost:8080/api';
+export const api = 'https://api.danfy.online:8080/danfy';
